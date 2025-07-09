4dc96ab2cf7fef551aa9168cf99990a998b54687
harshit <iamharshitsharma518@gmail.com>
2025-07-10 00:15:58 +0530

    hoistingvardone

--- javascript/variable.js
+++ javascript/variable.js
@@ -1,7 +1,15 @@
-var a = 10
-var a  = 20
-console.log(a)
+// var a = 10
+// var a  = 20
+// console.log(a)
 
 // var window m add hota h
-//block scope hah 
+//functional  scope hah 
 // redeclare kr skte hah
+
+//hoisting means declaration upar hota and initialisation niche so wo two parts me break ho jaata hah
+
+// console.log(a)
+// var a = 10;
+
+//ans undefined 
+

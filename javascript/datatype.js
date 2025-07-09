17bc6febe6e5c66d4173ba6382a315e912bd99a1
harshit <iamharshitsharma518@gmail.com>
2025-07-10 01:27:37 +0530

    datatypes

---
+++ javascript/datatype.js
@@ -0,0 +1,17 @@
+// let a = "harshit"
+// console.log(typeof(a))
+// let result = true
+// console.log(typeof(result))
+
+
+//== ===
+// console.log(5=="5") compare with conversion (not a strict) so ans is true
+// console.log(5=="5") compare without conversion(strict one)
+
+// console.log(typeof(NaN))
+
+
+
+
+
+

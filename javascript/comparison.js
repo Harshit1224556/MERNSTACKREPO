b8abeabbae53c6a04293e625f2023f83db9d6e7b
harshit <iamharshitsharma518@gmail.com>
2025-07-31 13:46:38 +0530

    lec 3 done for today

---
+++ javascript/comparison.js
@@ -0,0 +1,12 @@
+// console.log(2>3)
+
+//jb data type same ho to comparison me koi issue nh hah
+//issue tb hota ha jb data type change ho jaaye 
+// console.log("02" >1)
+// console.log("2" >1)
+
+//null ke saath jb hum comparison krte hah to jb kbhi wo 0 m change  kr deta 
+// aur jb kbhi Nan me
+
+console.log("2"==2)//dataype check nh krta
+console.log("2"===2)//datatype bh check krta hah isme

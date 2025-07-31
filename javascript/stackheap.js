b228dbe83eaf0dbab1a8ddf0477a786a0e18dc27
harshit <iamharshitsharma518@gmail.com>
2025-08-01 03:16:48 +0530

    string done

---
+++ javascript/stackheap.js
@@ -0,0 +1,31 @@
+//stack m hum primitive data type storekrta  hah  
+//heap m hum nonprimitive data tpe use krte hah 
+//stack m copy deta hah so original m changemh hota hah
+//heap m original m change ho jaata hahh
+
+//lets make a try by doing this
+
+
+// let name1 = "Harshit"
+// let name2 = name1
+// name2 = "Sharma"
+// console.log(name1)
+// console.log(name2)
+
+//copy gaya original m change nh hua  
+
+
+
+//now lets move to heap memory me jisme refernce jaaata hahh
+
+// const user1 = {
+//     email:"iamharshit@gmail.com"
+// }
+// let user2 = user1
+// user2.email="iam@gmail.com"
+// console.log(user1.email)
+// console.log(user2.email)
+
+//isme dono change ho gyaaaa
+
+

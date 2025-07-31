b47219a0f50b9a772dc6e327dfd17e58af553f90
harshit <iamharshitsharma518@gmail.com>
2025-08-01 04:21:23 +0530

    numbers done

--- javascript/number.js
+++ javascript/number.js
@@ -23,12 +23,24 @@
 //now going to explore some more maths values
 
 
-// console.log(Math.abs(-4)) 4
+// console.log(Math.abs(-4)) 4 random 
 // console.log(Math.round(4.6)) 5
 // console.log(Math.floor(4.2)) 4 niche walavalue
 // console.log(Math.ceil(4.2)) 5 upper vala value 
 // console.log(Math.max(2,3,4,6))
-// console.log(Math.min(2,3,4,6))
+// console.log(Math.min(2,3,4,6))Math.
 
 
-//something just important to generate random numbers
+
+
+//something just important to generatenumbers
+// console.log(Math.random())//random 0 se 1 k bich m number deta 
+// console.log(Math.random()*10)// usko 1 se  9 tak lana le ye kiya
+// console.log(Math.floor(random()* 10 + 1)) // ye hum isliye krte hah to avoid 0 wala caseee
+
+
+// const min = 10
+// const max = 20
+// console.log(Math.floor(Math.random()*(max-min+1) + min))
+
+// this is  best trick to generate random numbers

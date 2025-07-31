483015d17fe9424176f022aceeaaec0a295077a1
harshit <iamharshitsharma518@gmail.com>
2025-07-31 12:36:57 +0530

    lec 2 done

---
+++ javascript/operation.js
@@ -0,0 +1,12 @@
+let str1 = "harshit"
+let str2  = "sharma"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")//12
+// console.log("1" + 2)//12
+// console.log(1 + 2 + "2")//32
+// console.log("1" + 2 + 2)//122
+
+// console.log(true)
+// console.log(+true)

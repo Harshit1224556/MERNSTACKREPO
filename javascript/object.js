655736dfa084ba75a78d6cd148c2e0291f02bdfe
harshit <iamharshitsharma518@gmail.com>
2025-08-03 02:16:30 +0530

    object part 2 done

--- javascript/object.js
+++ javascript/object.js
@@ -27,5 +27,48 @@ jsuser.greeting  = function(){
 jsuser.greetingtwo = function(){
     console.log(`hello my name is ${this.name}`)
 }
-jsuser.greeting()
-jsuser.greetingtwo()
+// jsuser.greeting()
+// jsuser.greetingtwo()
+
+//different way to create object
+// const  tuser = new Object()
+// console.log(tuser)
+// const  tuser  = {}
+// tuser.name = "Harshit Sharma"
+// tuser.id = 123
+// console.log(tuser)
+
+
+const regularuser = {
+    email:"iamharshitsharma518@gmail.com",
+    fullname:
+    {
+        userfullname:{
+            firstname:"Harshit",
+            lastname:"Sharma"
+        }
+    }
+}
+// console.log(regularuser.fullname.userfullname.firstname)
+
+// this is the way to access object under object
+const obj1 = {
+    1:"A",
+    2:"B"
+}
+
+const obj2 = {
+    3:"C",
+    4:"D"
+}
+//merge two object
+// const obj3  = Object.assign({},obj1,obj2)
+// console.log(obj3)
+
+//best to  use is spread operator same as array
+
+const obj3 = {...obj1,...obj2}
+// console.log(obj3)
+console.log(Object.keys(jsuser))
+console.log(Object.values(jsuser))
+console.log(jsuser.hasOwnProperty('name'))

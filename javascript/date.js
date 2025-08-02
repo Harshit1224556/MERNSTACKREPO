1cac4edb2d9961c828d7380f23fa32ac042adf4e
harshit <iamharshitsharma518@gmail.com>
2025-08-02 13:17:40 +0530

    datesdone

---
+++ javascript/date.js
@@ -0,0 +1,29 @@
+// let mydate = new Date()
+
+// console.log(mydate.toString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleString())
+// console.log(typeof mydate)
+
+//javascript me month 0 se start hota hah
+// let mydate = new Date(2023,0,23)
+// console.log(mydate.toDateString())
+
+let newdate = new Date("2023-01-23")
+// console.log(newdate.toDateString())
+// console.log(newdate.toLocaleString())
+
+
+// let newdates  = Date.now();
+// console.log(newdates)
+// console.log(newdate.getTime())
+
+
+
+// let nwdate = new Date()
+// console.log(Math.floor(nwdate.getTime()/1000))
+// console.log(nwdate)
+// console.log(nwdate.getMonth()+1)
+// console.log(nwdate.getDay())
+

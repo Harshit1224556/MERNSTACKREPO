ee4582e037b9ba56cc531e43938a604aff0f9371
harshit <iamharshitsharma518@gmail.com>
2025-08-03 18:01:31 +0530

    functionpart1done

---
+++ javascript/function.js
@@ -0,0 +1,42 @@
+
+function saymyname(){
+    console.log("H")
+    console.log("a")
+    console.log("r")
+    console.log("s")
+    console.log("h")
+    console.log("i")
+    console.log("t")
+}
+
+// saymyname()
+
+
+// function addtwonumber(num1,num2){
+//     console.log(num1+num2)
+// }
+
+// addtwonumber(1,2)
+
+// function addtwonumber(num1,num2){
+//     return (num1+num2)
+// }
+// const res = addtwonumber(3,4)
+// console.log("Result",res)
+
+// function login(username){
+//     console.log(`my name is ${username}`)
+
+// }
+// login("Harshit Sharma")
+
+
+// function islogged(username){
+//     if(username===undefined){
+//        return "please enter a valid email"
+//     }
+
+//     return ` ${username} just logged in`
+// }
+// const  result = islogged("Harshit")
+// console.log(result)

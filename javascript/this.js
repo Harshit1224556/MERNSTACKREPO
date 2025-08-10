889d3048e2cfcf545383a0ef07c411d22021a1ab
harshit <iamharshitsharma518@gmail.com>
2025-08-11 00:52:48 +0530

    thisdone

--- javascript/this.js
+++ javascript/this.js
@@ -3,11 +3,18 @@ const user = {
     age:18,
     welcome:function(){
         console.log(`${this.name} welcome to this college`)
+        console.log(this)
     }
 }
 
-user.welcome()
-user.name = "Harsh"
-user.welcome()
+// user.welcome()
+// user.name = "Harsh"
+// user.welcome()
 
 //we use this to refer the current context
+
+// console.log(this)
+//here the  result is {} which means empty object iska mtlb ye hah ki global ke andar contextm 
+//ho nh hah to
+//lekin browser me wo window return kr degaaaaa
+

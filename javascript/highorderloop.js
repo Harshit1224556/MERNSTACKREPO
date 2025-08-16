95477625588dc827722108c402e2e5ab19c50851
harshit <iamharshitsharma518@gmail.com>
2025-08-16 22:35:59 +0530

    highorderloop

---
+++ javascript/highorderloop.js
@@ -0,0 +1,39 @@
+
+// const arr = [1,2,3,4,5]
+// for (const nums of arr) {
+//     console.log(nums)
+// }
+
+
+
+
+// let name = "HARSHIT"
+// for (const char of name) {
+    
+//     console.log(`each char of the name is ${char}`)
+// }
+
+
+
+//map 
+const map = new Map()
+map.set("a",1)
+map.set("b",2)
+map.set("c",3)
+// console.log(map.get("a"))
+
+//if i want to update the value of set
+
+// map.set("a",5)
+// console.log(map.get("a"))
+
+// console.log(map.size)
+// map.delete("a")
+// console.log(map.size)
+
+// console.log(map)
+
+// for (const [key,value] of map) {
+    
+//     console.log(`key - ${key} and value - ${value}`)
+// }

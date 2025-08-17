e0d4ae4b1dc6af6a488aa340dce6d62443285d57
harshit <iamharshitsharma518@gmail.com>
2025-08-17 17:20:32 +0530

    high order loop done

--- javascript/highorderloop.js
+++ javascript/highorderloop.js
@@ -16,10 +16,10 @@
 
 
 //map 
-const map = new Map()
-map.set("a",1)
-map.set("b",2)
-map.set("c",3)
+// const map = new Map()
+// map.set("a",1)
+// map.set("b",2)
+// map.set("c",3)
 // console.log(map.get("a"))
 
 //if i want to update the value of set
@@ -37,3 +37,74 @@ map.set("c",3)
     
 //     console.log(`key - ${key} and value - ${value}`)
 // }
+
+// const info = {
+//     name:"Harshit",
+//     roll :19
+// }
+// //in object in most of the cases we use for in loop
+
+// for (const key in info) {
+  
+//     console.log(`keys is ${key} and values are ${info[key]}`)
+// }
+
+
+const coding = ["js","cpp","nodejs","java","react"]
+
+
+// for (const key in coding) {
+    
+//     console.log(coding[key])
+// }
+
+//foreach loop me humme callback function likhna hota ha function havign no name 
+
+
+
+// coding.forEach(function(item){
+    
+//     console.log(item)
+// })
+
+
+
+// coding.forEach((item)=>{
+ 
+//      console.log(item)
+// })
+
+
+// function print(item){
+//     console.log(item)
+// }
+
+// coding.forEach(print)
+
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(item + " " + index +  " " + arr)
+// })
+
+
+const arr = [
+    {
+        language:"Javascript",
+        langfor:"js"
+    },
+    {
+        language:"Java",
+        langfor:"java"
+    },
+    {
+        language:"Python",
+        langfor:"py"
+    }
+
+]
+
+arr.forEach((item)=>{
+   console.log(item.language)
+})
+
+//this concept is used in when we are taking the data from database where each data is object

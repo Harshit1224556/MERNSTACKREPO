1ef945e267a9592cac0b60935ed16908645c03e0
harshit <iamharshitsharma518@gmail.com>
2025-08-22 06:27:50 +0530

    episode1done

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,1 +1,7 @@
-console.log(global)
+// console.log(global)
+// console.log(this)
+
+// console.log(globalThis)
+
+console.log(globalThis === global)
+
---
+++ reactjs/app.js
@@ -0,0 +1,26 @@
+
+
+// const heading  = React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},
+//         React.createElement("h1",{id:"seached"},"my nested heading")
+//     )
+// )
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+//if we want more than one sibling in the code we use passed it as the array 
+
+
+const heading  = React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},
+       [React.createElement("h1",{id:"firstsibling"},"this is my first siblings"),
+        React.createElement("h2",{id:"secondsibling"},"this is my second siblings")
+       ]
+
+
+    )
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
+
+//agar div root me phle se hi man lo koi element jaise h1 jiska value hah i am harshit 
+//us time par i am harshit is getting replaced by the parent div 

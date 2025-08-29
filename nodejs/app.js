8970aa72a7b4e557750abb1d06a25284cbe21b5a
harshit <iamharshitsharma518@gmail.com>
2025-08-29 14:51:56 +0530

    donewithassignmen

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,5 +3,23 @@
 
 // console.log(globalThis)
 
-console.log(globalThis === global)
+// console.log(globalThis === global)
+
+//to let the code on your repl mode what we have to do is that 
+// we have to type node on the terminal then write node 
+// the repl mode activated thats means read evaluate print loop 
+
+//node js is javascripr run time environment 
+//comne with v8 engine and special powers such as api calling database connection 
+//v8 engine runs on c++ application (code)
+//follows ecmascript
+//c++ machine codeee
+
+//i have to merge the file i use the require keyword to do that
+require("./xyx.js")
+require("./sum.js")
+console.log("heloo this is app.js")
+var a  = 10;
+var b = 20;
+sum(a,b)
 

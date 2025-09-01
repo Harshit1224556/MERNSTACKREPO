85ed6bc52e3a412a41c6fdc436ce6e6cdb67eaa9
harshit <iamharshitsharma518@gmail.com>
2025-09-01 08:59:05 +0530

    react project setup

---
+++ reactjs/fooddeliveryapp.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const Header = () => {
+ 
+    
+
+}
+
+const applayout = () => (
+ 
+<Header />
+
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Heading/>)

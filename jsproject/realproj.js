ed4192f39454a7162dd37dca931f8c1929bfb0a6
harshit <iamharshitsharma518@gmail.com>
2025-09-27 15:05:29 +0530

    just done with the projects

---
+++ jsproject/realproj.js
@@ -0,0 +1,15 @@
+var elem = document.querySelectorAll(".element")
+
+elem.forEach((val)=>{
+
+  val.addEventListener("mouseenter",()=>{
+            val.childNodes[3].style.opacity=1
+  })
+  val.addEventListener("mouseleave",()=>{
+            val.childNodes[3].style.opacity=0
+  })
+  val.addEventListener("mousemove",(dets)=>{
+            val.childNodes[3].style.left = dets.x + "px"
+            val.childNodes[3].style.top = dets.y + "px"
+  })
+})
